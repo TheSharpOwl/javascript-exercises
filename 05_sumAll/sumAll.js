9a2eee2196df21cfc3a6afb7bec2efca85f6edc9
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-04-02 17:06:25 +0300

    Solve ex 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  if (
+    first < 0 ||
+    second < 0 ||
+    typeof first != "number" ||
+    typeof second != "number"
+  )
+    return "ERROR";
 
+  let res = 0;
+  for (let i = first; i <= second; i++) {
+    res += i;
+  }
+  // for case second > first (and removing the equal case to not have twice the answer)
+  for (let i = second; i <= first && first !== second; i++) {
+    res += i;
+  }
+
+  return res;
 };
 
 // Do not edit below this line
