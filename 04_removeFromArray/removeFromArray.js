e91f9154581cf0b7297d4863cb46d9a1e162a3f9
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-04-02 15:57:18 +0300

    Solve ex 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+// includes does the job but thought it first it's the reason
+const myContains= function(arr, val) {
+    for(let i = 0; i < arr.length;i++){
+        if(arr[i] === val)
+        return true;
+    }
+    return false;
+}
+const removeFromArray = function () {
+  let args = Array.from(arguments);
+  let arr = args[0];
+  let toRemove = args.slice(1, args.length);
+  let newArr = [];
+  for(let i = 0; i < arr.length;i++)
+  {
+    if(!toRemove.includes(arr[i])) // or use myContains
+    {
+        newArr.push(arr[i]);
+    }
+  }
 
+  return newArr;
 };
 
 // Do not edit below this line
