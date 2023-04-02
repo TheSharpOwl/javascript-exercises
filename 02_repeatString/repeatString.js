247af7a49d3f20f4f46bc610772b6584c98c12f6
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-04-02 14:46:32 +0300

    Finish second exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (sentence, times) {
+  if (times < 0) return "ERROR";
+  if (times == 0) return "";
+  return sentence.repeat(times);
 };
 
 // Do not edit below this line
