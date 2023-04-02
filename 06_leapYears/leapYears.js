c2eaa4758b9c38a8edc0fc133ffd600a92d94182
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-04-02 17:22:22 +0300

    Finish ex 6.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (typeof year !== "number") {
+    return "ERROR";
+  }
+  return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
 };
 
 // Do not edit below this line
